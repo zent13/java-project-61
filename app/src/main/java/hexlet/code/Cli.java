bbc3e27da8d6a4d5006e006f55c0b57a3970c0c3
Afanasev Konstantin <zent13@mail.ru>
2022-11-27 08:42:17 +0300

    added Cli.java, where we ask user

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,15 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void whatYourName() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        String name = scanner.next();
+        System.out.println("Hello, " + name + "!");
+        scanner.close();
+    }
+}
